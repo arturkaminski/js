da73a90a6cd4ce353f4cb057f7b8f88c7b66f7d4
Artur Rudnicki <arudnicki@creatuity.com>
2022-07-19 13:11:39 +0200

    working version

--- records1.js
+++ records1.js
@@ -21,26 +21,30 @@ const recordCollection = {
 
 // Only change code below this line
 function updateRecords(records, id, prop, value) {
-  if((prop != records[id].tracks)&&(value != "")){
-    records[id].prop = value;
+  if((prop != "tracks")&&(value != "")){
+    records[id][prop] = value;
       console.log("if1\n");
   }
-  else if((prop == records[id].tracks)&&(records[id].hasOwnProperty(tracks))== false){
-    tracks = [];
-    records[id].tracks.push(value);
+  else if((prop == "tracks")&&(records[id].hasOwnProperty('tracks'))== false){
+    let tracks = [];
+     // tracks.push(value);
+      records[id].tracks = [value];
       console.log("if2\n");
   }
   else if((prop == records[id].tracks)&&(value != "")){
-    records.tracks.push(value);
+    records[id].tracks.push(value);
       console.log("if3\n");
   }
   else if(value == ""){
-    delete records[id].prop;
+    delete records[id][prop];
       console.log("if4\n");
   }
     console.log(records)
   return records;
 }
 
-
-updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+updateRecords(recordCollection, 2548, "tracks", "")
+//updateRecords(recordCollection, 2468, "tracks", "Free")
+//updateRecords(recordCollection, 2548, "artist", "")
+//updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+//updateRecords(recordCollection, 5439, "tracks", "Take a Chance on Me")
