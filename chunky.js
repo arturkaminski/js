8e1e91b44c2096fa79271ebae8138568f11ea2fa
Artur Rudnicki <arudnicki@creatuity.com>
2022-11-23 13:05:59 +0100

    i dont know why

---
+++ chunky.js
@@ -0,0 +1,41 @@
+function chunkArrayInGroups(arr, size) {
+  let arr1 = [[],[]];
+let arr2 = [];
+let arr3 = [];
+let l = arr.length;
+let x =0;
+let a =0;
+for (let i in arr){
+    if (x < size){
+        console.log("i=" + i);
+        console.log("arr[i]=" + arr[i]);
+        console.log("#######################");
+        arr1[a][x] = arr[i];
+        console.log("arr1 =" + arr1);
+        console.log("#######################");
+        x++;
+    }
+    else{
+
+        console.log(" else arr1 =");
+        console.log(arr1);
+        console.log("**********************");
+        console.log("else i=" + i);
+        a++;
+        x =0;
+        console.log("else a=" + a);
+        console.log("else x=" + x);
+        arr1[a][x] = arr[i];
+        console.log("else arr[i]=" + arr[i]);
+        console.log("else arr1 =" + arr1);
+
+    }
+    }
+        
+    
+    console.log("Result");
+    console.log(arr1);
+  return arr1;
+}
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
+//chunkArrayInGroups([0,1,2,3,4,5], 2);
