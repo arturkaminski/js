6da12a4611c0503f30cbd84f1d8c0e76502a8098
Artur Rudnicki <arudnicki@creatuity.com>
2022-12-01 23:18:38 +0100

    works, but not like it should

--- chunky.js
+++ chunky.js
@@ -1,5 +1,5 @@
 function chunkArrayInGroups(arr, size) {
-  let arr1 = [[],[]];
+  let arr1 = [[],[],[]];
 let arr2 = [];
 let arr3 = [];
 let l = arr.length;
@@ -7,11 +7,13 @@ let x =0;
 let a =0;
 for (let i in arr){
     if (x < size){
+        console.log("x=" + x);
         console.log("i=" + i);
         console.log("arr[i]=" + arr[i]);
         console.log("#######################");
         arr1[a][x] = arr[i];
-        console.log("arr1 =" + arr1);
+        console.log("arr1 =");
+        console.log(arr1);
         console.log("#######################");
         x++;
     }
@@ -23,12 +25,15 @@ for (let i in arr){
         console.log("else i=" + i);
         a++;
         x =0;
+        arr1[a][x] = arr[i];
+        x++;
         console.log("else a=" + a);
         console.log("else x=" + x);
-        arr1[a][x] = arr[i];
-        console.log("else arr[i]=" + arr[i]);
-        console.log("else arr1 =" + arr1);
-
+       // arr1[a][x] = arr[i];
+        console.log("else arr[i]=");
+        console.log(arr[i]);
+        console.log("else arr1 =");
+        console.log(arr1);
     }
     }
         
@@ -37,5 +42,5 @@ for (let i in arr){
     console.log(arr1);
   return arr1;
 }
-chunkArrayInGroups(["a", "b", "c", "d"], 2);
-//chunkArrayInGroups([0,1,2,3,4,5], 2);
+chunkArrayInGroups(["a", "b", "c", "d", "e", "f"], 2);
+chunkArrayInGroups([0,1,2,3,4,5], 2);
